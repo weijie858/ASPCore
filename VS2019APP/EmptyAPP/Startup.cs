ea88b258192ed995e11e82568294d9833c152f5c
weij <317543501@qq.com>
2019-12-10 17:22:58 +0800

    tijiao

--- VS2019APP/EmptyAPP/Startup.cs
+++ VS2019APP/EmptyAPP/Startup.cs
@@ -15,6 +15,7 @@ namespace EmptyAPP
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+           // services.confi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
