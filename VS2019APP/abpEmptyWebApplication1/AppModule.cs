f8819bd2b8bf422ede95d58abd622b5993ad9e65
317543501@qq.com <317543501@qq.com>
2020-03-28 12:00:12 +0800

    添加appmodule

--- VS2019APP/abpEmptyWebApplication1/AppModule.cs
+++ VS2019APP/abpEmptyWebApplication1/AppModule.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder; //GetApplicationBuilder(
+using Microsoft.Extensions.Hosting;  //IsDevelopment
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Volo.Abp;//ApplicationInitializationContext
+using Volo.Abp.AspNetCore.Mvc; //AbpAspNetCoreMvcModule
+using Volo.Abp.Modularity;  //DependsOn
 
 namespace abpEmptyWebApplication1
 {
-    public class AppModule
+    [DependsOn(typeof(AbpAspNetCoreMvcModule))]
+    public class AppModule : AbpModule
     {
+        public override void OnApplicationInitialization(
+            ApplicationInitializationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+            var env = context.GetEnvironment();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseMvcWithDefaultRouteAndArea();
+        }
     }
 }
