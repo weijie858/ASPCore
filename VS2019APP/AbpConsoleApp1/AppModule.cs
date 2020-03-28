ab72ed1ccc4e887a5a52cffe5bdda4da6629e306
317543501@qq.com <317543501@qq.com>
2020-03-28 11:29:54 +0800

    tijiao

---
+++ VS2019APP/AbpConsoleApp1/AppModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Modularity;  //AbpModule
+
+namespace AbpConsoleApp1
+{
+    public class AppModule:AbpModule
+    {
+    }
+}
---
+++ VS2019APP/AbpConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Volo.Abp;  //AbpApplicationFactory
+
+namespace AbpConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var application = AbpApplicationFactory.Create<AppModule>())
+            {
+                application.Initialize();
+
+                Console.WriteLine("Press ENTER to stop application...");
+                Console.ReadLine();
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
